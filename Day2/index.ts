0c685dc695ed09b3c28c6fc29e471e64b7b35c85
Roger Hamilton <roger.max.hamilton@gmail.com>
2021-12-03 08:10:02 -0600

    refactor

--- Day2/index.ts
+++ Day2/index.ts
@@ -1,40 +1,28 @@
 import { readLines } from '../utils';
 
-interface Direction {
+interface Nav {
   dir: 'forward' | 'down' | 'up'
   value: number
 }
 
-const parseLine = (line: string): Direction => {
+const parseLine = (line: string): Nav => {
   const [dir, value] = line.split(' ');
-  return { dir: dir as Direction['dir'], value: parseInt(value) };
+  return { dir: dir as Nav['dir'], value: parseInt(value) };
 };
 
 interface State1 { pos: number, depth: number }
 
-const step1 = ({ pos, depth }: State1, { dir, value }: Direction): State1 => {
-  switch (dir) {
-    case 'down':
-      return { pos, depth: depth + value };
-    case 'up':
-      return { pos, depth: Math.max(0, depth - value) };
-    case 'forward':
-      return { pos: pos + value, depth };
-  }
-}
+const step1 = ({ pos, depth }: State1, { dir, value }: Nav): State1 =>
+  dir === 'down' ? { pos, depth: depth + value } :
+  dir === 'up' ? { pos, depth: depth - value } :
+  { pos: pos + value, depth };
 
 interface State2 extends State1 { aim: number }
 
-const step2 = ({ pos, depth, aim }: State2, { dir, value }: Direction): State2 => {
-  switch (dir) {
-    case 'down':
-      return { pos, depth, aim: aim + value };
-    case 'up':
-      return { pos, depth, aim: aim - value };
-    case 'forward':
-      return { pos: pos + value, depth: depth + (aim * value), aim };
-  }
-}
+const step2 = ({ pos, depth, aim }: State2, { dir, value }: Nav): State2 =>
+  dir === 'down' ? { pos, depth, aim: aim + value } :
+  dir === 'up' ? { pos, depth, aim: aim - value } :
+  { pos: pos + value, depth: depth + (aim * value), aim };
 
 const main = async () => {
   const lines = await readLines('./Day2/input.txt');
