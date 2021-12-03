e92a4fce25d7e05149bb77746647e55ea9416fa7
Roger Hamilton <roger.max.hamilton@gmail.com>
2021-12-03 09:17:02 -0600

    use reduce

--- Day3/index.ts
+++ Day3/index.ts
@@ -2,43 +2,40 @@ import { readLines } from '../utils';
 
 const parseLine = (line: string) => parseInt(line, 2);
 
-let step1 = (nums: number[]) => (bit: number) => {
+let step1 = (nums: number[], bit: number) => {
   const mask = 1 << bit;
   const results = nums.filter(num => num & mask);
   return results.length >= nums.length / 2 ? mask : 0;
 }
 
 const part1 = (nums: number[], len: number) => {
-  const commonBit = step1(nums);
-  const gamma = [...Array(len).keys()].reduce((acc, c) => acc + commonBit(c), 0);
+  const gamma = [...Array(len).keys()]
+    .reduce((acc, c) => acc + step1(nums, c), 0);
+
   const epsilon = ~gamma & ((1 << len) - 1);
   return gamma * epsilon;
 }
 
-const part2 = (nums: number[], len: number) => {
-  let bit = len - 1;
-  let ox = [...nums];
-  while (ox.length > 1) {
-    const mask = 1 << bit;
-    const common = step1(ox)(bit);
-    ox = ox.filter(num => (num & mask) === common);
-    bit--;
-  }
-
-  bit = len - 1;
-  let co2 = [...nums];
-  while (co2.length > 1) {
-    const mask = 1 << bit;
-    const common = step1(co2)(bit);
-    co2 = co2.filter(num => (num & mask) !== common);
-    bit--;
-  }
-
-  return ox[0] * co2[0];
+const step2 = (comp: (a: number, b: number) => boolean) => (nums: number[], bit: number) => {
+  const mask = 1 << bit;
+  const common = step1(nums, bit);
+  return nums.filter(num => comp((num & mask), common));
 }
 
-(async () => {
-  const lines = await readLines('./Day3/input.txt');
+const part2 = (nums: number[], len: number) => {
+  const [ox] = [...Array(len).keys()]
+    .reverse()
+    .reduce(step2((a, b) => a === b), nums);
+    
+  const [co2] = [...Array(len).keys()]
+    .reverse()
+    .reduce(step2((a, b) => a !== b), nums);
+
+  return ox * co2;
+}
+
+const main = async () => {
+  const lines = await readLines('./Day3/test.txt');
   const len = lines[0].length;
   const input = lines.map(parseLine);
   
@@ -47,4 +44,6 @@ const part2 = (nums: number[], len: number) => {
 
   const p2 = part2(input, len);
   console.log(`Part 2: ${p2}`);
-})();
+};
+
+main();
