2076417b890d81fd1d80701f6eb7b2ea967f25cb
Roger Hamilton <roger.max.hamilton@gmail.com>
2021-12-03 08:55:00 -0600

    refactor

--- Day3/index.ts
+++ Day3/index.ts
@@ -5,12 +5,12 @@ const parseLine = (line: string) => parseInt(line, 2);
 let step1 = (nums: number[]) => (bit: number) => {
   const mask = 1 << bit;
   const results = nums.filter(num => num & mask);
-  return results.length >= nums.length / 2 ? 1 : 0;
+  return results.length >= nums.length / 2 ? mask : 0;
 }
 
 const part1 = (nums: number[], len: number) => {
   const commonBit = step1(nums);
-  const gamma = [...Array(len).keys()].reverse().reduce((acc, c) => (acc << 1) + commonBit(c), 0);
+  const gamma = [...Array(len).keys()].reduce((acc, c) => acc + commonBit(c), 0);
   const epsilon = ~gamma & ((1 << len) - 1);
   return gamma * epsilon;
 }
@@ -21,7 +21,7 @@ const part2 = (nums: number[], len: number) => {
   while (ox.length > 1) {
     const mask = 1 << bit;
     const common = step1(ox)(bit);
-    ox = ox.filter(num => (num & mask ? 1 : 0) === common);
+    ox = ox.filter(num => (num & mask) === common);
     bit--;
   }
 
@@ -30,14 +30,14 @@ const part2 = (nums: number[], len: number) => {
   while (co2.length > 1) {
     const mask = 1 << bit;
     const common = step1(co2)(bit);
-    co2 = co2.filter(num => (num & mask ? 1 : 0) !== common);
+    co2 = co2.filter(num => (num & mask) !== common);
     bit--;
   }
 
   return ox[0] * co2[0];
 }
 
-const main = async () => {
+(async () => {
   const lines = await readLines('./Day3/input.txt');
   const len = lines[0].length;
   const input = lines.map(parseLine);
@@ -47,6 +47,4 @@ const main = async () => {
 
   const p2 = part2(input, len);
   console.log(`Part 2: ${p2}`);
-}
-
-main();
+})();
