19308926af50207d01b437936d3fe0c07fecd26a
Roger Hamilton <roger.max.hamilton@gmail.com>
2021-12-05 16:04:13 -0600

    refactor

--- Day4/index.ts
+++ Day4/index.ts
@@ -1,7 +1,6 @@
 import { splitBy, toInt, readLines } from '../utils';
 
-const parseBalls = (line: string) =>
-  line.split(',').map(toInt);
+const parseBalls = (line: string) => line.split(',').map(toInt);
 
 const parseBoard = (board: string[]) =>
   board.map(row => row.split(/\s+/).filter(Boolean).map(toInt));
@@ -12,8 +11,8 @@ const isWinner = (board: number[][], balls: number[]) =>
   board.some(row => row.every(b => balls.includes(b)))
   || asCols(board).some(row => row.every(b => balls.includes(b)));
 
-const allRuns = (balls: number[]) =>
-  [...Array(balls.length).keys()].map(i => balls.slice(0, i + 1))
+const allRuns = (balls: number[]) => [...Array(balls.length).keys()]
+  .map(i => balls.slice(0, i + 1))
 
 const part1 = (boards: number[][][], balls: number[]) => {
   const [currentBalls, winner] = allRuns(balls)
@@ -27,12 +26,8 @@ const part1 = (boards: number[][][], balls: number[]) => {
 }
 
 const part2 = (boards: number[][][], balls: number[]) => {
-  let loosers = boards;
-  for(const run of allRuns(balls)) {
-    loosers = loosers.filter(board => !isWinner(board, run));
-    if (loosers.length === 1) break;
-  }
-  
+  const loosers = allRuns(balls)
+    .reduce((l, run) => l.length < 2 ? l : l.filter(b => !isWinner(b, run)), boards);
   return part1(loosers, balls);
 }
 
