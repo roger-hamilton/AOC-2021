9abc4d82bb7c1dd4004a9c42f38275c658ac4e51
Roger Hamilton <roger.max.hamilton@gmail.com>
2021-12-06 08:14:51 -0600

    refactor

--- Day6/index.ts
+++ Day6/index.ts
@@ -3,15 +3,12 @@ import { toInt, readLines, memo } from '../utils';
 const calcPop = memo((input: number, days: number): number =>
   days <= input ? 1: calcPop(6, days - input - 1) + calcPop(8, days - input - 1))
   
-const part1 = (input: number[], days: number = 80): number =>
+const sumPop = (input: number[], days: number = 80): number =>
   input.reduce((acc, curr) => acc + calcPop(curr, days), 0);
 
-const main = async () => {
-  const [line] = await readLines('./Day6/input.txt');
-  const input = line.split(',').map(toInt);
-  
-  console.log(`Part 1: ${part1(input)}`);
-  console.log(`Part 2: ${part1(input, 256)}`);
-};
-
-main();
+readLines('./Day6/input.txt')
+  .then(([line]) => line.split(',').map(toInt))
+  .then(input => {
+    console.log(`Part 1: ${sumPop(input)}`);
+    console.log(`Part 2: ${sumPop(input, 256)}`);
+  });
