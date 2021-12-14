ed6eaf7bbc9b49aa4966f0fc5a182bbe14064350
Roger Hamilton <roger.max.hamilton@gmail.com>
2021-12-14 16:14:34 -0600

    refactor

--- Day14/index.ts
+++ Day14/index.ts
@@ -1,5 +1,4 @@
-import { memo } from './../utils';
-import { readLines, toInt, splitBy } from '../utils';
+import { readLines, memo, range, splitBy } from '../utils';
 
 type Rule = { pattern: string; insert: string }
 type Input = { seed: string; rules: Rule[] }
@@ -11,50 +10,45 @@ const parseRule = (line: string): Rule => {
 
 const parseInput = (lines: string[]) => {
   const [[seed], rules] = splitBy(lines, '');
-  return {
-    seed,
-    rules: rules.map(parseRule)
-  }
+  return { seed, rules: rules.map(parseRule) };
 }
 
-const applyAndCount = (seed: string, rules: Rule[], itr: number = 10) => {
-  const ruleLookup = Object.fromEntries(rules.map(r => [r.pattern, `${r.pattern[0]}${r.insert}${r.pattern[1]}`]));
+const addTo = (record: Record<string, number>, [key, value]: [string, number]) => {
+  record[key] = (record[key] ?? 0) + value;
+  return record;
+}
 
-  const merge = (a: Record<string, number>, b: Record<string, number>) =>
-    [...Object.entries(a), ...Object.entries(b)].reduce((acc, [key, val]) => {
-      acc[key] = (acc[key] ?? 0) + val;
-      return acc;
-    }, {} as Record<string, number>);
+const merge = (a: Record<string, number>, b: Record<string, number>) =>
+  [...Object.entries(b)].reduce(addTo, {...a});
+
+const inc = (record: Record<string, number>, key: string) => addTo(record, [key, 1]);
+const dec = (record: Record<string, number>, key: string) => addTo(record, [key, -1]);
+
+const applyAndCount = (seed: string, rules: Rule[], itr: number = 10) => {
+  const ruleLookup = Object.fromEntries(rules.map(r => [
+    r.pattern,
+    `${r.pattern[0]}${r.insert}${r.pattern[1]}`
+  ]));
 
   let inner = memo((str: string, itr: number): Record<string, number> => {
-    if (str.length !== 2) throw new Error('Invalid string length');
-    if (itr === 0) {
-      return [...str].reduce((acc, val) => {
-        acc[val] = (acc[val] || 0) + 1;
-        return acc;
-      }, {} as Record<string, number>);
-    }
+    if (itr === 0) return [...str].reduce(inc, {});
     const next = ruleLookup[str];
-    const left = inner(next.slice(0, 2), itr - 1);
-    const right = inner(next.slice(1, 3), itr - 1);
+    const res = merge(
+      inner(next.slice(0, 2), itr - 1),
+      inner(next.slice(1, 3), itr - 1));
 
-    const res = merge(left, right);
+    // remove the overlapping center char
     res[next[1]]--;
-    return res
+    return res;
   });
 
-  const results = [];
-  for(let i = 1; i < seed.length; i++) {
-    results.push(inner(seed.slice(i - 1, i + 1), itr));
-  }
+  const results = range(1, seed.length - 1)
+    .map(i => inner(seed.slice(i - 1, i + 1), itr))
+    .reduce(merge, {});
 
-  const res = results.reduce((acc, val) => merge(acc, val), {} as Record<string, number>);
-  for(const c of seed) {
-    res[c]--;
-  }
-  res[seed[0]]++
-  res[seed[seed.length - 1]]++;
-  return res;
+  return range(1, seed.length - 2)
+    .map(i => seed[i])
+    .reduce(dec, results);
 }
 
 const part1 = (input: Input, itr = 10): number => {
