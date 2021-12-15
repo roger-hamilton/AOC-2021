56ce464b38b29335e891c3678969be94ad52a7ce
Roger Hamilton <roger.max.hamilton@gmail.com>
2021-12-15 11:28:54 -0600

    refactor

--- Day15/index.ts
+++ Day15/index.ts
@@ -1,10 +1,11 @@
-import { readLines, memo, range, splitBy, toInt } from '../utils';
+import { readLines, memo, range, splitBy, toInt, chain } from '../utils';
 
 const parseInput = (lines: string[]): number[][] =>
   lines.map(line => line.split('').map(toInt));
 
 const nextToDirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-const getNextTo = (x: number, y: number): [number, number][] => nextToDirs.map(([dx, dy]) => [x + dx, y + dy]);
+const getNextTo = (x: number, y: number): [number, number][] =>
+  nextToDirs.map(([dx, dy]) => [x + dx, y + dy]);
 
 const calcRisks = (grid: number[][]) => {
   const toCheck: [number, number][] = [];
@@ -17,9 +18,8 @@ const calcRisks = (grid: number[][]) => {
     if (curr === undefined) return;
 
     const near = getNextTo(x, y).filter(([x, y]) => riskGrid[y]?.[x] !== undefined);
-
     if (near.length === 0) return;
-    
+
     const minRisk = near
       .map(([x, y]) => riskGrid[y]?.[x]?.minRiskTo)
       .reduce((acc, curr) => Math.min(acc, curr), Infinity);
@@ -40,7 +40,6 @@ const calcRisks = (grid: number[][]) => {
   }
 
   update(0, 0);
-  
   while (toCheck.length) {
     const [x, y] = toCheck.shift()!;
     toCheckSet.delete(`${x},${y}`);
@@ -74,22 +73,24 @@ const expandGrid = (grid: number[][]) => {
   return newGrid;
 }
 
-const part1 = (grid: number[][]) => {
-  const riskGrid = calcRisks(grid);
-  return riskGrid[riskGrid.length - 1][riskGrid[0].length - 1];
-}
+const last = <T>(arr: T[][]) => arr[arr.length - 1][arr[arr.length - 1].length - 1];
 
-const part2 = (grid: number[][]) => {
-  console.time('expanded search');
-  const riskGrid = calcRisks(expandGrid(grid));
-  console.timeEnd('expanded search');
-  return riskGrid[riskGrid.length - 1][riskGrid[0].length - 1];
-}
+const part1 = (grid: number[][]) =>
+  chain(grid)
+    .pipe(calcRisks)
+    .pipe(last)
+    .valueOf();
+
+const part2 = (grid: number[][]) =>
+  chain(grid)
+    .pipe(expandGrid)
+    .pipe(calcRisks)
+    .pipe(last)
+    .valueOf()
 
 readLines('./Day15/input.txt')
   .then(parseInput)
   .then(input => {
     console.log(`Part 1: ${part1(input)}`);
-
     console.log(`Part 2: ${part2(input)}`);
   });
