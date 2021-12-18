430052e096a20e263be040283f008210eb393739
Roger Hamilton <roger.max.hamilton@gmail.com>
2021-12-18 17:41:03 -0600

    fix part 2

--- Day17/index.ts
+++ Day17/index.ts
@@ -1,4 +1,4 @@
-import { range, toInt } from "../utils";
+import { range } from "../utils";
 
 type Vec2<T = number> = [T, T];
 type Target = Vec2<Vec2>;
@@ -17,14 +17,13 @@ const step = ({ pos: [x, y], vel: [dx, dy] }: State): State => {
   return { pos: [x, y], vel: [dx, dy] };
 }
 
-function* locs(dx: number, dy: number, maxSteps = 100) : IterableIterator<Vec2> {
+function* locs(dx: number, dy: number) : IterableIterator<Vec2> {
   let state: State = { pos: [0, 0], vel: [dx, dy] };
   let n = 0;
   while(true) {
     state = step(state);
     yield state.pos;
     n++;
-    if (n > maxSteps) break;
   }
 }
 
@@ -42,32 +41,30 @@ const landsIn = (target: Target) => (dx: number, dy: number) => {
   const [minY, maxY] = target[1];
   const [minX, maxX] = target[0];
 
-  const inYZone = locsAbove(dx, dy, minY)
-    .filter(([, y]) => y <= maxY)
-    .filter(([x]) => x >= minX && x <= maxX);
-
-  return inYZone.length > 0;
+  for (const [x, y] of locsAbove(dx, dy, minY)) {
+    if (y >= minY && y <= maxY && x >= minX && x <= maxX) return true;
+  }
+  return false;
 }
 
 const part2 = (target: Target) => {
   const [[minX, maxX], [minY, maxY]] = target;
 
-  const dyRange = range(-minY * 2, minY * 2);
-  const dxRange = range(0, maxX * 2);
+  const dyRange = range(-minY, minY);
+  const dxRange = range(0, maxX);
   const check = landsIn(target);
-  
-  const arr = [];
+
+  let count = 0;
   for(const dy of dyRange) {
     for(const dx of dxRange) {
-      if (check(dx, dy)) arr.push([dx, dy]);
+      if (check(dx, dy)) count++;
     }
   }
-  return arr.length;
+  return count;
 }
 
 const test = [[20,30], [-10, -5]] as Target;
 const input = [[34,67], [-215, -186]] as Target;
 
 console.log(`Part 1: ${part1(input)}`);
-// 1808 -> too low
 console.log(`Part 2: ${part2(input)}`);
